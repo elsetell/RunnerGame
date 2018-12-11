c9526e6faee1829840a3f44ba3be0709c8b0ee92
Oleg Cheban <odcheban@gmail.com>
2018-12-11 12:55:10 +0300

    add VR_mode

--- RunnerGame/Assets/Scripts/Character/CharacterInputController.cs
+++ RunnerGame/Assets/Scripts/Character/CharacterInputController.cs
@@ -33,9 +33,24 @@ public class CharacterInputController : MonoBehaviour {
         transform.Translate(Vector3.forward * Time.deltaTime * playerSettings.speedMove);
     }
 
+#if UNITY_STANDALONE
     void RotateCharacter()
     {
         float rotateX = Input.GetAxisRaw("Horizontal") * playerSettings.speedRotate;
+        AddAngle(rotateX);
+    }
+#endif
+
+#if UNITY_ANDROID
+    void RotateCharacter()
+    {
+        float rotateX = Input.gyro.rotationRate.z * playerSettings.speedRotate;
+        AddAngle(rotateX);
+    }
+#endif
+
+    void AddAngle(float rotateX)
+    {
         float angle = transform.localEulerAngles.y;
         angle = (angle > 180) ? angle - 360 : angle;
         if ((rotateX > 0 && angle < playerSettings.angleRotateMax) || (rotateX < 0 && angle > -playerSettings.angleRotateMax))
--- RunnerGame/Assets/Scripts/ObjectFollowTo.cs
+++ RunnerGame/Assets/Scripts/ObjectFollowTo.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectFollowTo : MonoBehaviour {
-    [SerializeField]
-    private Transform player;
-    [SerializeField]
-    private Vector3 offset;
+    public Transform player;
+    public Vector3 offset;
 
+#if UNITY_ANDROID
+    private void Start()
+    {
+        UnityEngine.XR.InputTracking.disablePositionalTracking = true;
+        Input.gyro.enabled = true;
+
+        Transform skyObj = transform.GetChild(0);
+        skyObj.SetParent(null);
+        skyObj.gameObject.AddComponent<ObjectFollowTo>();
+        ObjectFollowTo sky = skyObj.GetComponent<ObjectFollowTo>();
+        sky.player = player;
+        sky.offset.z = -320;
+    }
+#endif
     void LateUpdate()
     {
         transform.position = player.position + offset;
