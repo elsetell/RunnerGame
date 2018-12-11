9d55c33d3e9b171bf80862b2f39ad670711606c0
Oleg Cheban <odcheban@gmail.com>
2018-12-11 11:17:29 +0300

    Add ObjectPool

---
+++ RunnerGame/Assets/Scripts/Map/PoolObjects.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolObjects : MonoBehaviour {
+    public PoolSettings poolSettings;
+    private Dictionary<string, List<GameObject>> poolDict;
+    private Transform objectsParent;
+
+    public void CreatePool()
+    {
+        poolDict = new Dictionary<string, List<GameObject>>();
+        objectsParent = new GameObject("Pool").transform;
+        foreach (Pool pool in poolSettings.pools)
+        {
+            List<GameObject> objectPool = new List<GameObject>();
+            poolDict.Add(pool.name, objectPool);
+
+            for (int i = 0; i < pool.size; i++)
+            {
+                AddObject(pool);
+            }
+        }
+    }
+    public GameObject AddObject(Pool pool)
+    {
+        GameObject obj = Instantiate(pool.prefab);
+        obj.SetActive(false);
+        poolDict[pool.name].Add(obj);
+        obj.transform.SetParent(objectsParent);
+        return obj;
+    }
+    public GameObject AddObject(string name)
+    {
+        Pool pool = GetPoolByName(name);
+        return (pool != null) ? AddObject(pool) : null;
+    }
+
+    public GameObject GetObject(string name, Vector3 pos)
+    {
+        if (!poolDict.ContainsKey(name))
+            return null;
+        GameObject objectToSpawn = GetObjectOfPool(name);
+        objectToSpawn.transform.position = pos;
+        objectToSpawn.SetActive(true);
+        return objectToSpawn;
+    }
+
+    private Pool GetPoolByName(string name)
+    {
+        foreach (Pool pool in poolSettings.pools)
+            if (pool.name == name)
+                return pool;
+        return null;
+    }
+    private GameObject GetObjectOfPool(string name)
+    {
+        foreach (GameObject obj in poolDict[name])
+            if (!obj.activeInHierarchy)
+                return obj;
+        return AddObject(name);
+    }
+}
